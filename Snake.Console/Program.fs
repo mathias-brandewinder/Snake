981769a601caa6a4ee8b39a07a12901541996944
Mathias Brandewinder <mathias.brandewinder@gmail.com>
2014-04-01 11:33:23 -0700

    Design exploration

---
+++ Snake.Console/Program.fs
@@ -0,0 +1,59 @@
+﻿open System
+open Snake
+open Snake.Game
+
+type MoveResult =
+    | Exit
+    | Collision
+    | OK
+
+[<EntryPoint>]
+let main argv = 
+
+    printfn "%A" argv
+
+    let rng = Random ()
+
+    let move () =
+        let r = rng.NextDouble ()
+        if r < 0.3 then Some(Left)
+        elif r < 0.6 then Some(Right)
+        else None
+
+    let result (b:Board) (s:Snake) (p:Pos) =
+        if p.X > b.Xmax || p.X < b.Xmin || p.Y > b.Ymax || p.Y < b.Ymin
+        then Exit
+        elif (snd s) |> List.exists (fun x -> x = p)
+        then Collision
+        else OK
+
+    let render (p:Pos) =
+        Console.SetCursorPosition(p.X,p.Y)
+        Console.ForegroundColor <- System.ConsoleColor.Red
+        Console.Write "*"
+
+    let exit () =
+        Console.Write "EXIT!"
+        Console.ReadLine () |> ignore
+
+    let collision () =
+        Console.Write "COLLISION!"
+        Console.ReadLine () |> ignore
+
+    let rec loop (b:Board) (s:Snake) =
+        let m = move ()
+        let (d,hd) = update s m
+        match (result b s hd) with
+        | Exit -> exit ()
+        | Collision -> collision ()
+        | OK ->  
+            System.Threading.Thread.Sleep 20          
+            hd |> render
+            let s' = d,hd::(snd s)
+            loop b s'
+
+    let snake = N, [{X=20;Y=20}]
+    let board = { Xmin = 0; Xmax = 50; Ymin = 0; Ymax = 50 }
+    loop board snake
+
+    0 // return an integer exit code
---
+++ Snake/Core.fs
@@ -0,0 +1,57 @@
+﻿namespace Snake
+
+type Pos = { X: int; Y: int; }
+
+type Dir = 
+    | N
+    | S
+    | E
+    | W
+
+type Move =
+    | Left
+    | Right
+
+type Snake = Dir * Pos list
+
+type State = { Snake1: Snake; Snake2: Snake; }
+
+type Board = { Xmin: int; Xmax: int; Ymin: int; Ymax: int }
+
+module Game =
+
+    let rev (d:Dir) =
+        match d with
+        | N -> S
+        | W -> E
+        | S -> N
+        | E -> W
+
+    let left (d:Dir) =
+        match d with
+        | N -> W
+        | W -> S
+        | S -> E
+        | E -> N
+
+    let right (d:Dir) = d |> rev |> left
+
+    let move (x:Pos) (d:Dir) =
+        match d with
+        | N -> { x with Y = x.Y - 1 }
+        | S -> { x with Y = x.Y + 1 }
+        | E -> { x with X = x.X + 1 }
+        | W -> { x with X = x.X - 1 }
+                
+    let turn (d: Dir) (m: Move option) =
+        match m with
+        | None -> d
+        | Some(x) ->
+            match x with
+            | Left -> left d
+            | Right -> right d
+
+    let update (s: Snake) (m: Move option) =
+        let (d,(x::xs)) = s // ignore empty snake
+        let d' = turn d m
+        d',(move x d')
